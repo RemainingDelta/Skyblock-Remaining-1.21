84b5afe78f5f92d7a1a7d3547eb881a9a386d125
Shiven Ajwaliya <shiven.ajwaliya@yahoo.com>
2026-03-21 17:03:07 -0400

    7-Feature add config menu

--- src/client/java/com/remainingdelta/ModMenuIntegration.java
+++ src/client/java/com/remainingdelta/ModMenuIntegration.java
@@ -1,14 +1,22 @@
 package com.remainingdelta;
 
+import com.remainingdelta.gui.ConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
-import com.remainingdelta.config.ModConfig;
 
+/**
+ * Integrates SkyblockRemaining with Mod Menu.
+ * Provides a config screen factory so the mod appears in the Mod Menu list
+ * with a clickable config button.
+ */
 public class ModMenuIntegration implements ModMenuApi {
 
+  /**
+   * Returns the factory used to create the mod's config screen.
+   * Opens the custom ConfigScreen when the user clicks the config button in Mod Menu.
+   */
   @Override
   public ConfigScreenFactory<?> getModConfigScreenFactory() {
-    return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+    return parent -> new ConfigScreen();
   }
 }
--- src/client/java/com/remainingdelta/config/ModConfig.java
+++ src/client/java/com/remainingdelta/config/ModConfig.java
@@ -4,9 +4,18 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
+/**
+ * Defines the configuration schema for SkyblockRemaining.
+ * Fields in this class are automatically serialized to and from
+ * a JSON file by AutoConfig. Add new config fields here as needed.
+ */
 @Config(name = "skyblockremaining")
 public class ModConfig implements ConfigData {
 
+  /**
+   * Whether the HUD overlay is enabled.
+   * Defaults to true.
+   */
   @Comment("Whether the HUD is enabled")
   public boolean hudEnabled = true;
 }
